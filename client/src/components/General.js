d37ab94d8746df8656f94e604fc4962212200ce5
masterbwong <45401417+masterbwong@users.noreply.github.com>
2020-06-05 00:37:25 -0700

    Add files via upload

--- client/src/components/General.js
+++ client/src/components/General.js
@@ -6,33 +6,58 @@ import Bar from './Charts/Bar';
 import Doughnut from './Charts/Doughnut';
 import StatsCard from './Charts/StatsCard';
 import EnergyDemand from './Charts/EnergyDemand';
+import TotalChart from './Charts/totUChart';
+import ChargeOpt from './Charts/chargeOpt';
 
 
 class Home extends Component {
 
   render() {
     return (
-      <Container >
-        <br></br>
-        <h1>General System Performance</h1>
-        <hr></hr>
-        <StatsCard />
-        <Row>
-          <Col >
-            <Doughnut />
-          </Col>
-          <Col>
-            <Bar />
-          </Col>
+      <Container>
           <br></br>
-        </Row>
-        <hr></hr>
-        <br></br>
-        <Row>
-          <Col>
-            <EnergyDemand />
-          </Col>
-        </Row>
+          <h1>General System Performance</h1>
+          <hr></hr>
+        
+          <Row>
+            <StatsCard />
+          </Row>
+
+
+          <Col md="12" className="card">
+            <br></br>
+            <h5 style = {{ textALign: "middle" }}> Charger Completion Status</h5>
+            <span className="block-example border border-dark"></span>
+            <br></br>
+            <span className="block-example border border-dark">
+            <TotalChart/>
+            <br></br>
+            </span>
+            </Col>
+                 
+            <br></br>
+            <Col md="12" className="card">
+              <br></br>
+              <h5 style = {{ textALign: "middle" }}> Charger Option</h5>
+              <span className="block-example border border-dark"></span>
+              <br></br>
+              <span className="block-example border border-dark">
+              <ChargeOpt/>
+              <br></br>
+              </span>
+            </Col>
+
+        
+          <br></br>
+          
+          <hr></hr>
+          <br></br>
+          <Row>
+            <Col md="12">
+              <EnergyDemand />
+            </Col>
+          </Row>
+        
       </Container>
     );
   }
--- client/src/components/Optimizer.js
+++ client/src/components/Optimizer.js
@@ -7,33 +7,68 @@ import Completionstatus from './Charts/Completionstatus';
 import Carbons from './Charts/Carbons';
 import CardOpt from './Charts/CardOpt';
 
+
 class Optimizer extends Component {
 
-    render() {
-        return (
+    render(){
+        return(
             <Container >
-                <br></br>
-                <h1>Metric Tools Page</h1>
-                <hr></hr>
+              <br></br>
+              <h1 style = {{ textALign: "middle"}}>Metric Tools Page</h1>
+              <hr></hr>
+              <br></br>
                 <Row>
+
                     <Col md="12" className="card">
-                        <CardOpt />
+                      <CardOpt/>
                     </Col>
                     <br></br>
-                    <Col md="12" className="card">
-                        <Completionstatus />
-                    </Col>
+                    
+                    <Col md="12" className="card"> 
+                    <br ></br>
+                    <h5 style = {{ textALign: "middle" }}> Charger Completion Status</h5>
+                    <span className="block-example border border-dark"></span>
+                    <br></br>
+                    <span className="block-example border border-dark">
+                        <Completionstatus/>
+                    </span>
+                    </Col>    
+                    
+
                 </Row>
                 <hr></hr>
                 <Col md="12" className="card">
-                    <Electricitycost />
                     <br></br>
+                    <h5 style = {{ textALign: "middle" }}> Electricity cost</h5> 
+                    <span className="block-example border border-dark"></span>
+                    <br></br>
+                   <span className="block-example border border-dark">
+                     <Electricitycost/>
+                   </span>
                 </Col>
-                <br></br>
+               
                 <hr></hr>
-                <Row>
-                    <Carbons />
-                </Row>
+                <br></br>
+                <Col md="12" className="card">
+                  
+                      
+                        <h5 style = {{ textALign: "middle" }}> Carbon Emissions</h5> 
+                        <span className="block-example border border-dark"></span>
+                      
+    
+                
+               
+                  
+                  <br></br>
+                        
+                        <Carbons/>
+                        
+                </Col>
+                    
+             
+
+
+                   
             </Container>
         );
     }
