7e428e897ff369b1af481e4846f9f01c47140b42
masterbwong <45401417+masterbwong@users.noreply.github.com>
2020-06-05 00:36:21 -0700

    Add files via upload

---
+++ client/src/components/Charts/chargeOpt.js
@@ -0,0 +1,71 @@
+import React, { PureComponent } from 'react';
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+import axios from "axios";
+
+
+export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
+
+  state = {
+    data:[]
+  }
+
+  componentDidMount() {
+    axios.get("https://api.calplug.club/api.php?collection=chargerCount")
+      .then(res => {
+        
+        
+        let tmpnum = 0;
+        var d = new Date();        
+        let curHour = d.getHours();
+        let count = 7;
+        let tempList = [];
+        console.log(curHour);
+        for(let i = curHour; i >= 0; i--)
+        {
+          
+          if(count > 0)
+          {
+            for(let j = 0; j < res.data.result.length; j++) {
+              tmpnum = res.data.result[j].number;
+              if(tmpnum === i) {
+                tempList.push(res.data.result[j]);
+                console.log(res.data.result[j]);
+                break;
+              }
+            }
+            if(i == 0) i = 24;
+            count--;
+          } else {
+            break;
+          }
+        }
+        this.setState({ data: tempList.reverse() });
+        
+       
+      })
+    }
+   
+  render() {
+    return (
+      <BarChart
+        width={1000}
+        height={300}
+        data={this.state.data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Time" />
+        <YAxis label={{ value: 'Time', angle: -90, position: 'insideLeft', offset: -5 }} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="chargeNow" fill="#8884d8" />
+        <Bar dataKey="optimize" fill="#82ca9d" />
+      </BarChart>
+    );
+  }
+}
